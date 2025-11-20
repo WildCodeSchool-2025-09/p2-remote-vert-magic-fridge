e176fa907e0cefe1d7df6a835190fe0cc443b138
Yuliana Krasulya <krasulyayuliana@gmail.com>
2025-11-20 22:15:47 +0100

    Add Blog page and blog images, Header component for all pages exept RecipeSheet, animated title, and update App and router

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router";
 
+import { TypeAnimation } from "react-type-animation";
+
 import headerImage1 from "../assets/images/header_image_1.jpg";
 import headerImage2 from "../assets/images/header_image_2.jpg";
 import headerImage3 from "../assets/images/header_image_3.jpg";
@@ -23,6 +25,27 @@ export default function Header() {
 	return (
 		<header className="header">
 			<img src={headerImage} alt="Header banner" className="header-image" />
+			<div className="header-overlay">
+				<TypeAnimation
+					sequence={[
+						"STRANGER FRIDGE",
+						2000,
+						"",
+						500,
+						"FRIGO DE LA MAMA",
+						2000,
+						"",
+						500,
+						"NO TIME TO EAT",
+						2000,
+					]}
+					speed={50}
+					wrapper="h1"
+					repeat={0}
+					cursor={false}
+					className="header-title"
+				/>
+			</div>
 		</header>
 	);
 }

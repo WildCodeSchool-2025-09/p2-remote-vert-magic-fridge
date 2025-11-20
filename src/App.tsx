2ebd4552e2c758d4dfe674374113519d35c19529
Yuliana Krasulya <krasulyayuliana@gmail.com>
2025-11-20 15:32:19 +0100

    update avec dev actuel

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Link, Outlet } from "react-router";
 import "./App.css";
 import "./index.css";
 import "./styles/reset.css";
+import NavBar from "./components/NavBar";
 import RecipeFilter from "./components/RecipeFilter";
 
 function App() {
 	return (
 		<>
+			<NavBar />
 			<Link to="/">Home</Link>
 			<Link to="/favorite">Favorite</Link>
 			<Outlet />
